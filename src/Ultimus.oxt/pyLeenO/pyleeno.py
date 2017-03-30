641f109b6610465136d703c89b91c9aece97fa74
giuseppe vizziello <giuserpe@giuserpe.it>
2017-03-31 00:27:15 +0200

    Cambia la subversion in .dev

--- src/Ultimus.oxt/pyLeenO/pyleeno.py
+++ src/Ultimus.oxt/pyLeenO/pyleeno.py
@@ -4125,7 +4125,7 @@ Al termine dell'impotazione controlla la voce con tariffa """ + dict_articoli.ge
 #VARIABILI GLOBALI:
 Lmajor= 3 #'INCOMPATIBILITA'
 Lminor= 16 #'NUOVE FUNZIONALITA'
-Lsubv= "0"#'CORREZIONE BUGS
+Lsubv= "1.dev"#'CORREZIONE BUGS
 noVoce = ('Livello-0-scritta', 'Livello-1-scritta', 'livello2 valuta', 'comp Int_colonna')
 siVoce = ('Comp Start Attributo', 'comp progress', 'comp 10 s','Comp End Attributo', )
 siVoce_R = ('Comp Start Attributo_R', 'comp 10 s_R','Comp End Attributo_R', )

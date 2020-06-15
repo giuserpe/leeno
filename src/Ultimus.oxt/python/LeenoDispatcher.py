6946f74cd77ddef1f7ca62b4a10eda723666d0e6
Massimo Del Fedele <max@veneto.com>
2020-06-15 12:33:03 +0200

    Added exception messages

--- src/Ultimus.oxt/python/LeenoDispatcher.py
+++ src/Ultimus.oxt/python/LeenoDispatcher.py
@@ -13,6 +13,23 @@ from os.path import isfile, join
 import unohelper
 from com.sun.star.task import XJobExecutor
 
+import uno
+import traceback
+from com.sun.star.awt import MessageBoxButtons as MSG_BUTTONS
+def msgbox(*, Title='Errore interno', Message=''):
+    """ Create message box
+        type_msg: infobox, warningbox, errorbox, querybox, messbox
+        http://api.libreoffice.org/docs/idl/ref/interfacecom_1_1sun_1_1star_1_1awt_1_1XMessageBoxFactory.html
+    """
+    ctx = uno.getComponentContext()
+    sm = ctx.getServiceManager()
+    toolkit = sm.createInstance('com.sun.star.awt.Toolkit')
+    parent = toolkit.getDesktopWindow()
+    buttons=MSG_BUTTONS.BUTTONS_OK
+    type_msg='errorbox'
+    mb = toolkit.createMessageBox(parent, type_msg, buttons, Title, str(Message))
+    return mb.execute()
+
 # set this to 1 to enable debugging
 # set to 0 before deploying
 ENABLE_DEBUG = 1
@@ -23,7 +40,7 @@ ENABLE_DEBUG = 1
 DISABLE_CACHE = 1
 
 if ENABLE_DEBUG == 1:
-    pass 
+    pass
 
 
 
@@ -80,35 +97,57 @@ class Dispatcher(unohelper.Base, XJobExecutor):
         This function gets called when a menu item is selected
         or when a basic function calls PyScript()
         '''
-        # reload all Leeno Modules
-        if DISABLE_CACHE != 0:
-            reloadLeenoModules()
+        try:
+            # reload all Leeno Modules
+            if DISABLE_CACHE != 0:
+                reloadLeenoModules()
 
-        # menu items are passed as module.function
-        # so split them in 2 strings
-        ModFunc = arg.split('.')
+            # menu items are passed as module.function
+            # so split them in 2 strings
+            ModFunc = arg.split('.')
 
-        # locate the module from its name and check it
-        module = importlib.import_module(ModFunc[0])
-        if module is None:
-            print("Module '", ModFunc[0], "' not found")
-            return
+            # locate the module from its name and check it
+            module = importlib.import_module(ModFunc[0])
+            if module is None:
+                print("Module '", ModFunc[0], "' not found")
+                return
 
-        # reload the module if we don't want the cache
-        # if DISABLE_CACHE != 0:
-        #     importlib.reload(module)
+            # reload the module if we don't want the cache
+            # if DISABLE_CACHE != 0:
+            #     importlib.reload(module)
 
-        # locate the function from its name and check it
-        func = getattr(module, ModFunc[1])
-        if func is None:
-            print("Function '", ModFunc[1], "' not found in Module '", ModFunc[0], "'")
-            return
+            # locate the function from its name and check it
+            func = getattr(module, ModFunc[1])
+            if func is None:
+                print("Function '", ModFunc[1], "' not found in Module '", ModFunc[0], "'")
+                Dialogs.Exclamation(
+                    Title="Errore interno",
+                    Text=f"Funzione '{ModFunc[1]}' non trovata nel modulo '{ModFunc[0]}'")
+                return
 
-        # call the handler, depending of number of arguments
-        if len(self.args) == 0:
-            func()
-        else:
-            func(self.args)
+            # call the handler, depending of number of arguments
+            if len(self.args) == 0:
+                func()
+            else:
+                func(self.args)
+
+        except Exception as e:
+            # msg = traceback.format_exc()
+            print("sys.exc_info:", sys.exc_info())
+            sysinfo = sys.exc_info()
+            exceptionClass = sysinfo[0].__name__
+            message = str(sysinfo[1])
+            tb = sysinfo[2]
+            tbInfo = traceback.extract_tb(tb, 1)[0]
+            function = tbInfo.name
+            line = tbInfo.lineno
+            file = os.path.split(tbInfo.filename)[1]
+            msg = (
+                message + "\n\n" +
+                "File:     '" + file + "'\n" +
+                "Line:     '" + str(line) + "'\n\n" +
+                "Function: '" + function + "'\n\n")
+            msgbox(Title="Errore interno", Message=msg)
 
 
 g_ImplementationHelper = unohelper.ImplementationHelper()

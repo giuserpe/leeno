85fa80738d9bb747fd452a6d8aaab498396d892c
giuserpe <giuserpe@giuserpe.it>
2020-06-29 08:59:27 +0200

    Debug di MENU_copia_sorgente_per_git() MENU_avvia_IDE()
    Corregge il path dei sorgenti.

--- src/Ultimus.oxt/python/pythonpath/pyleeno.py
+++ src/Ultimus.oxt/python/pythonpath/pyleeno.py
@@ -722,7 +722,7 @@ def MENU_copia_sorgente_per_git():
             dest = os.getenv("HOMEDRIVE") + os.getenv(
                 "HOMEPATH") + '\\' + src_oxt + '\\leeno\\src\\Ultimus.oxt\\'
         else:
-            dest = 'w:/_dwg/ULTIMUSFREE/_SRC/leeno/src/Ultimus.oxt'
+            dest = 'w:\\_dwg\\ULTIMUSFREE\\_SRC\\leeno\\src\\Ultimus.oxt'
         subprocess.Popen(
             'w: && cd w:/_dwg/ULTIMUSFREE/_SRC/leeno/src/Ultimus.oxt && "C:/Program Files/Git/git-bash.exe"',
             shell=True,
@@ -787,7 +787,7 @@ def avvia_IDE():
             dest = os.getenv("HOMEDRIVE") + os.getenv(
                 "HOMEPATH") + '\\' + src_oxt + '\\leeno\\src\\Ultimus.oxt\\'
         else:
-            dest = 'w:/_dwg/ULTIMUSFREE/_SRC/leeno/src/Ultimus.oxt'
+            dest = 'w:\\_dwg\\ULTIMUSFREE\\_SRC\\leeno\\src\\Ultimus.oxt'
         subprocess.Popen('explorer.exe ' +
                          dest, 
                          shell=True,

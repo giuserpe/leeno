e6777e12896dc83fbb3a417fd7e827b74e9de1b4
giuserpe <giuserpe@giuserpe.it>
2021-10-02 13:04:49 +0200

    Debug computo_terra_terra(): Raggruppava colonne già raggruppate

--- src/Ultimus.oxt/python/pythonpath/pyleeno.py
+++ src/Ultimus.oxt/python/pythonpath/pyleeno.py
@@ -7295,6 +7295,7 @@ def computo_terra_terra():
     oCellRangeAddr.Sheet = iSheet
     oCellRangeAddr.StartColumn = 46
     oCellRangeAddr.EndColumn = 1023
+    oSheet.ungroup(oCellRangeAddr, 0)
     oSheet.group(oCellRangeAddr, 0)
     
     oSheet.getCellRangeByPosition(33, 0, 1023, 0).Columns.IsVisible = False
@@ -9220,58 +9221,21 @@ from xml.etree.ElementTree import ElementTree, Element, SubElement, Comment, tos
 # ~from xml.etree.ElementTree import ElementTree, ParseError
 from xml.etree.ElementTree import ElementTree, Element, SubElement, Comment, tostring
 def MENU_debug():
-    LI.MENU_basilicata_2020()
-    return
-    rigenera_parziali(False)
-    # ~catalogo_stili_cella()
-    return
-    LeenoAnalysis.MENU_impagina_analisi()
-    # ~set_area_stampa()
-    # ~MENU_sistema_pagine()
-    return
-    # ~oS = oDoc.getSheets().getByName('dcf')
-    # ~lst = []
-    # ~for i in range(1, 17514):
-        # ~lst.append(oS.getCellByPosition(0, i).String)
-
+    oDoc = LeenoUtils.getDocument()
+    
     oSheet = oDoc.getSheets().getByName(oDoc.CurrentController.ActiveSheet.Name)
-    lr = LeggiPosizioneCorrente()[1]
-    row = LeenoSheetUtils.prossimaVoce(oSheet, lr, 1)
+    DLG.mri(oSheet.getCellByPosition(2, 1))
+
+    return
     lr = SheetUtils.getLastUsedRow(oSheet) + 1
-    # ~lr = oDoc.CurrentSelection.CellAddress.Row
-    for i in reversed (range(1, lr)):
-        # ~if oSheet.getCellByPosition(7, i).String == "CAM":
-            # ~oSheet.getCellByPosition(0, i).String = oSheet.getCellByPosition(0, i).String + ".CAM"
+    
+    for el in reversed(range (4,  lr)):
 
-        try:
-            lst = oSheet.getCellByPosition(0, i).String.split('.')
-            if len (lst) == 3:
-                if len(lst[2]) < 3:
-                    oSheet.getCellByPosition(0, i).String = lst[0] + '.' + lst[1] + '.0' + lst[2]# + '.0' + lst[3]# + '.' + lst[4]
-        except:
-            pass
-        # ~if oSheet.getCellByPosition(0, i).String in lst:
-        
+        if oSheet.getCellByPosition(2, el).String == '--':
+            oSheet.getRows().removeByIndex(el, 1)
 
-        # ~if oSheet.getCellByPosition(1, i).Type.value != 'EMPTY':
-            # ~oSheet.getCellByPosition(8, i).String = "CAM"
+    return
 
-        # ~ if oSheet.getCellByPosition(2, i).Type.value == 'EMPTY' or \
-         # ~ oSheet.getCellByPosition(2, i).String == 'Descrizione estesa':
-        # ~ if oSheet.getCellByPosition(5, i).Type.value != 'EMPTY':
-            # ~ oSheet.getCellByPosition(4, i).Value = oSheet.getCellByPosition(5, i).Value * oSheet.getCellByPosition(7, i).Value 
-
-        # ~ if oSheet.getCellByPosition(0, i).String == oSheet.getCellByPosition(0, i+1).String:
-            # ~ _gotoCella(0, i)
-            # ~ return
-        # ~ else:
-            # ~_gotoCella(0, i)
-            # ~return
-            # ~DLG.chi(oSheet.getCellByPosition(1, i).String.split('.'))
-            # ~oSheet.getRows().removeByIndex(i, 1)
-
-        # ~else:
-            # ~DLG.chi('ihio')
     return
             # ~newl.append('<version value="' + new + '" />')
     Ldev = str (int(f.readline().split('LeenO-')[1].split('-')[0].split('.')[-1]) + 1)

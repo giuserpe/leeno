3a7274cb85f428e2519148615f072fc1f3ecb86d
giuserpe <giuserpe@giuserpe.it>
2022-11-01 22:09:11 +0100

    Aggiunge MENU_ValdAosta(). Ma spesso il file da trattare non è ordinato.

--- src/Ultimus.oxt/python/pythonpath/LeenoImport.py
+++ src/Ultimus.oxt/python/pythonpath/LeenoImport.py
@@ -498,13 +498,14 @@ def MENU_emilia_romagna():
     LeenoUtils.DocumentRefresh(True)
 
 ########################################################################
-
 def MENU_umbria():
     '''
     Adatta la struttura del prezzario rilasciato dalla regione Umbria
     
     Il risultato ottenuto va inserito in Elenco Prezzi.
     '''
+    # ~SheetUtils.MENU_unisci_fogli()
+    PL.GotoSheet("unione_fogli")
     oDoc = LeenoUtils.getDocument()
     LeenoUtils.DocumentRefresh(False)
     oSheet = oDoc.CurrentController.ActiveSheet
@@ -533,6 +534,24 @@ def MENU_umbria():
     LeenoUtils.DocumentRefresh(True)
 
 ########################################################################
+
+
+def MENU_ValdAosta():
+    '''Non va: spesso il file di origine non è ordinato'''
+    oDoc = LeenoUtils.getDocument()
+    LeenoUtils.DocumentRefresh(False)
+    oSheet = oDoc.CurrentController.ActiveSheet
+    oSheet.Columns.insertByIndex(3, 1)
+    primo_nome = oSheet.Name
+    oSheet.Name = 'COMPUTO'
+    LeenoSheetUtils.MENU_elimina_righe_vuote()
+    oSheet.Name = oSheet.Name
+    fine = SheetUtils.getLastUsedRow(oSheet) + 1
+    for i in range(0, fine):
+        if len(oSheet.getCellByPosition(0, i).String.split('.')) == 2:
+            madre = oSheet.getCellByPosition(1, i).String
+        elif len(oSheet.getCellByPosition(0, i).String.split('.')) == 3:
+            oSheet.getCellByPosition(1, i).String = madre + '\n- ' + oSheet.getCellByPosition(1, i).String
 
 
 def MENU_Piemonte():

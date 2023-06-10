2c998dcf774af5ec1534c2b227b662c4461d9439
giuserpe <giuserpe@giuserpe.it>
2023-06-10 18:45:33 +0200

    Riprovo con la gestione della tabella pesi e misure

--- src/Ultimus.oxt/python/pythonpath/LeenoTabelle.py
+++ src/Ultimus.oxt/python/pythonpath/LeenoTabelle.py
@@ -1,19 +1,15 @@
 '''
 Gestione tabelle Pesi e Misure
-*** DA REVISIONARE ***
-
-con questa versione LibreOffice va in freeze e il file tabelle.ods rimane aperto più volte
-
 '''
-# ~import uno
-# ~import pyleeno as PL
+import uno
+import pyleeno as PL
 
-# ~import LeenoUtils
-# ~import LeenoDialogs as DLG
-# ~import DocUtils
+import LeenoUtils
+import LeenoDialogs as DLG
+import DocUtils
 
 
-# ~class tabelle_dati:
+def tabelle_dati():
     
     # ~def __init__ (self):
         # ~""" Class initialiser """
@@ -23,78 +19,82 @@ con questa versione LibreOffice va in freeze e il file tabelle.ods rimane aperto
     # ~except:
         # ~pass
 
-    # ~filename = uno.fileUrlToSystemPath(PL.LeenO_path()) + '/data/tabelle.ods'
-    # ~oDoc = DocUtils.loadDocument(filename, Hidden=True)
+    filename = uno.fileUrlToSystemPath(PL.LeenO_path()) + '/data/tabelle.ods'
+    oDoc = DocUtils.loadDocument(filename, Hidden=True)
 
-    # ~tabelle = {'Tondo per c.a.': 'tondo_ca',
-        # ~'Reti elettrosaldate' : 'reti_els',
-        # ~'Pesi specifici' : 'pesi_specifici',
-        # ~'Categorie' : 'categorie'
-        # ~}
+    tabelle = {'Tondo per c.a.': 'tondo_ca',
+        'Reti elettrosaldate' : 'reti_els',
+        'Pesi specifici' : 'pesi_specifici',
+        'Categorie' : 'categorie'
+        }
 
-    # ~fogli = oDoc.getSheets().getElementNames()
+    fogli = oDoc.getSheets().getElementNames()
     
-    # ~psm = LeenoUtils.getComponentContext().ServiceManager
-    # ~dp = psm.createInstance('com.sun.star.awt.DialogProvider')
+    psm = LeenoUtils.getComponentContext().ServiceManager
+    dp = psm.createInstance('com.sun.star.awt.DialogProvider')
 
-    # ~oDlg = dp.createDialog(
-        # ~"vnd.sun.star.script:UltimusFree2.Dialog_tabelle?language=Basic&location=application"
-    # ~)
-    # ~combobox = oDlg.getControl('ComboBox1')
-    # ~sString = combobox
-    # ~sString.Text = fogli[0]
-    # ~listbox = oDlg.getControl('ListBox1')
+    oDlg = dp.createDialog(
+        "vnd.sun.star.script:UltimusFree2.Dialog_tabelle?language=Basic&location=application"
+    )
 
-    # ~def init():
-        # ~combobox = tabelle_dati.combobox
-        # ~listbox = tabelle_dati.listbox
+    combobox = oDlg.getControl('ComboBox1')
+    # ~DLG.chi(fogli)
 
-        # ~combobox.addItems(tabelle_dati.fogli, 0)
-        # ~nome = combobox.Text
+    combobox.Text = fogli[0]
 
-        # ~oSheet = tabelle_dati.oDoc.Sheets.getByName(nome)
-        # ~Dati = oSheet.getCellRangeByName(tabelle_dati.tabelle[nome]).DataArray
-        # ~lista = []
-        # ~for el in Dati:
-            # ~lista.append(list(el)[0])
-        # ~listbox.addItems(lista, 0)
-        # ~# tabelle_dati.oDlg.execute()
-        # ~# try:
-            # ~# tabelle_dati.oDlg.endExecute()
-        # ~# except:
-            # ~# pass
-        # ~# DLG.chi(combobox.Text)
-        # ~return
+    listbox = oDlg.getControl('ListBox1')
 
-    # ~def compila():
-        # ~# return
-        # ~combobox = tabelle_dati.combobox
-        # ~listbox = tabelle_dati.listbox
-        # ~listbox.removeItems(0, len(listbox.Items))
-        
-        # ~nome = combobox.Text
-        # ~# DLG.mri(combobox)
-        # ~# return
-        
-        # ~oSheet = tabelle_dati.oDoc.Sheets.getByName(nome)
-        # ~Dati = oSheet.getCellRangeByName(tabelle_dati.tabelle[nome]).DataArray
+# ~def init():
+    # ~combobox = tabelle_dati.combobox
+    # ~listbox = tabelle_dati.listbox
 
-        # ~lista = []
-        # ~for el in Dati:
-            # ~lista.append(list(el)[0])
+    combobox.removeItems(0, len(combobox.Items))
+    combobox.addItems(fogli, 0)
+    nome = combobox.Text
+    # ~DLG.chi(nome)
 
-        # ~listbox.addItems(lista, 0)
-        # ~# tabelle_dati.oDlg.endExecute()
-        # ~tabelle_dati.oDlg.execute()
-        # ~return
-    # ~def ok():
+    oSheet = oDoc.Sheets.getByName(nome)
+    Dati = oSheet.getCellRangeByName(tabelle[nome]).DataArray
+    lista = []
+    for el in Dati:
+        lista.append(list(el)[0])
+    listbox.addItems(lista, 0)
+    # ~tabelle_dati.oDlg.execute()
+    # ~try:
         # ~tabelle_dati.oDlg.endExecute()
-        # ~tabelle_dati.oDlg.dispose()
-        # ~tabelle_dati.oDoc.dispose()
-        # ~return
-    # ~pass
+    # ~except:
+        # ~pass
+    # ~DLG.chi(combobox.Text)
+    return
 
-# ~def tabella_compila():
-    # ~tabelle_dati.compila()
-# ~def tabella_ok():
-    # ~tabella_dati.ok()
+# ~def compila():
+    # ~return
+    # ~combobox = tabelle_dati.combobox
+    # ~listbox = tabelle_dati.listbox
+    listbox.removeItems(0, len(listbox.Items))
+    
+    nome = combobox.Text
+    # ~DLG.mri(combobox)
+    # ~return
+    
+    oSheet = oDoc.Sheets.getByName(nome)
+    Dati = oSheet.getCellRangeByName(tabelle[nome]).DataArray
+
+    lista = []
+    for el in Dati:
+        lista.append(list(el)[0])
+
+    listbox.addItems(lista, 0)
+    tabelle_dati.oDlg.endExecute()
+    tabelle_dati.oDlg.execute()
+    # ~tabelle_dati.oDoc.dispose()
+    return
+def ok():
+    # ~DLG.chi(tabelle_dati.oDlg.getControl(listbox).SelectedItem)
+    tabelle_dati.oDlg.endExecute()
+    tabelle_dati.oDlg.dispose()
+    # ~tabelle_dati.oDoc.dispose()
+    return
+def tabella_compila():
+    # ~tabelle_dati()
+    pass

cb5676df7cd74c04363a458567ed9a427cdc3cc3
giuserpe <giuserpe@giuserpe.it>
2024-03-06 19:11:01 +0100

    Aggiorna fissa()
    Aggiornate le chiamate a freezeAtPosition per assicurare che il freeze
    sia impostato correttamente in base al foglio attivo.

--- src/Ultimus.oxt/python/pythonpath/pyleeno.py
+++ src/Ultimus.oxt/python/pythonpath/pyleeno.py
@@ -9641,14 +9641,18 @@ def fissa():
             'VARIANTE',
             'Elenco Prezzi'
     ):
+        _gotoCella(0, 3)
         oDoc.CurrentController.freezeAtPosition(0, 3)
     elif oSheet.Name in ('CONTABILITA'):
+        _gotoCella(0, 3)
         oDoc.CurrentController.freezeAtPosition(0, 3)
     elif oSheet.Name in ('Analisi di Prezzo'):
+        _gotoCella(0, 2)
         oDoc.CurrentController.freezeAtPosition(0, 2)
     elif oSheet.Name in ('Registro', 'SAL'):
+        _gotoCella(0, 1)
         oDoc.CurrentController.freezeAtPosition(0, 1)
-    # ~_gotoCella(lcol, lrow)
+    _gotoCella(lcol, lrow)
 
 
 ########################################################################
@@ -10285,37 +10289,7 @@ def ESEMPIO_create_progress_bar():
 # ~########################################################################
 import LeenoImport
 def MENU_debug():
-    LeenoUtils.initGlobalVars({
-        'Lmajor': 3,        # INCOMPATIBILITA'
-        'Lminor': 24,       # NUOVE FUNZIONALITA'
-        'Lsubv': "0.dev",       # CORREZIONE BUGS
-        # ~'Lmajor': 3,        # INCOMPATIBILITA'
-        # ~'Lminor': 23,       # NUOVE FUNZIONALITA'
-        # ~'Lsubv': "0",       # CORREZIONE BUGS
-
-        'noVoce': ('Livello-0-scritta', 'Livello-1-scritta', 'livello2 valuta',
-                   'comp Int_colonna', 'Ultimus_centro_bordi_lati',
-                   'comp Int_colonna_R_prima'),
-
-        'stili_cat': ('Livello-0-scritta', 'Livello-1-scritta', 'livello2 valuta', 
-                    'comp Int_colonna_R_prima', 'comp Int_colonna'),
-
-        'stili_computo': ('Comp Start Attributo', 'comp progress', 'comp 10 s',
-                        'Comp End Attributo'),
-
-        'stili_contab': ('Comp Start Attributo_R', 'comp 10 s_R', 'uuuuu',
-                         'Comp End Attributo_R', 'Comp TOTALI'),
-        'stili_analisi': ('Analisi_Sfondo', 'An.1v-Att Start', 'An-1_sigla',
-                          'An-lavoraz-desc', 'An-lavoraz-Cod-sx', 'An-lavoraz-desc-CEN',
-                          'An-sfondo-basso Att End'),
-        'stili_elenco':  ('EP-Cs', 'EP-aS'),
-
-        'codice_da_cercare': '',
-        'sUltimus': '',
-
-        'sblocca_computo': 0,
-
-    })
+    fissa()
     return
 
 

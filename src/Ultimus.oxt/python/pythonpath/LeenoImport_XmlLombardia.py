6f74cb24863eb99cd8e803784f276697b038c9a9
giuserpe <giuserpe@giuserpe.it>
2024-06-09 22:14:39 +0200

    Aggiunge la funzione clean_text() e migliora la pulizia del testo in sistema_cose()
    - Aggiunge la funzione clean_text() per eseguire le sostituzioni e pulire il
      testo.
    - Sposta le sostituzioni comuni nella funzione clean_text() per evitare la
      ripetizione del codice.
    - Aggiorna sistema_cose() per utilizzare la funzione clean_text() durante la
      pulizia del testo nelle celle.
    - Migliora la gestione degli spazi e delle righe vuote multiple.
    - Introduce pyleeno come PL.
    - Aggiorna parseXML() e parseXML1() per migliorare la gestione delle categorie
      e degli articoli.

--- src/Ultimus.oxt/python/pythonpath/LeenoImport_XmlLombardia.py
+++ src/Ultimus.oxt/python/pythonpath/LeenoImport_XmlLombardia.py
@@ -1,6 +1,7 @@
 import LeenoImport
 import xml.etree.ElementTree as ET
 import LeenoDialogs as DLG
+import pyleeno as PL
 
 # ~from com.sun.star.sheet.CellFlags import \
     # ~VALUE, DATETIME, STRING, ANNOTATION, FORMULA, HARDATTR, OBJECTS, EDITATTR, FORMATTED
@@ -35,72 +36,94 @@ def parseXML(data, defaultTitle=None):
     # elemento radice dell' albero XML
     root = LeenoImport.stripXMLNamespaces(data)
 
-    voci = root.find('voci')
-    voci = voci.find('voci')
+    voci = root.find('voci/voci')
     rifvoce = voci.find('riferimenti_voce')
-    titolo = rifvoce.find('autore').text + ' ' + rifvoce.find('invigore').text + ' ' + rifvoce.find('anno').text
 
+    try:
+        titolo = f"{rifvoce.find('autore').text} {rifvoce.find('invigore').text} {rifvoce.find('anno').text}"
+    except AttributeError:
+        titolo = f"{rifvoce.find('autore').text} {rifvoce.find('anno').text}"
+    
+    try:
+        titolo += f"_{rifvoce.find('edizione').text}"
+    except AttributeError:
+        pass
+    
     artList = {}
     superCatList = {}
     catList = {}
-    
+
     # prendo il suffisso da aggiungere al codice categorie
-    suff = root[0][0].getchildren()[1].attrib['CMPcodifica_voce'][:8]
+    try:
+        suff = root[0][0].getchildren()[1].attrib['CMPcodifica_voce'][:8]
+    except:
+        suff = root[0][0].getchildren()[1].attrib['codice_voce'][:8]
 
-    voci = root[0]
+    voci = root.find('voci')
     for voce in voci:
-        dettaglio_voce = voce.getchildren()[1]
+        dettaglio_voce = list(voce)[1]
+        ###
         # estrae supercategoria e categoria
-        codiceSuperCat = suff + dettaglio_voce.attrib['codifica_I_livello_voce']
-        superCat = dettaglio_voce.attrib['declaratoria_I_livello_voce']
-        if not codiceSuperCat in superCatList:
-            superCatList[codiceSuperCat] = superCat
-
-        codiceSuperCat = suff + dettaglio_voce.attrib['codifica_II_livello_voce']
-        superCat = dettaglio_voce.attrib['declaratoria_II_livello_voce']
-        if not codiceSuperCat in superCatList:
-            superCatList[codiceSuperCat] = superCat
-
-        codiceSuperCat = suff + dettaglio_voce.attrib['codifica_III_livello_voce']
-        superCat = dettaglio_voce.attrib['declaratoria_III_livello_voce']
-        if not codiceSuperCat in superCatList:
-            superCatList[codiceSuperCat] = superCat
-
-        codiceSuperCat = suff + dettaglio_voce.attrib['codifica_IV_livello_voce']
-        superCat = dettaglio_voce.attrib['declaratoria_IV_livello_voce']
-        if not codiceSuperCat in superCatList:
-            superCatList[codiceSuperCat] = superCat
-
         try:
+            codiceSuperCat = suff + dettaglio_voce.attrib['codifica_I_livello_voce']
+            superCat = dettaglio_voce.attrib['declaratoria_I_livello_voce']
+            if not codiceSuperCat in superCatList:
+                superCatList[codiceSuperCat] = superCat
+
+            codiceSuperCat = suff + dettaglio_voce.attrib['codifica_II_livello_voce']
+            superCat = dettaglio_voce.attrib['declaratoria_II_livello_voce']
+            if not codiceSuperCat in superCatList:
+                superCatList[codiceSuperCat] = superCat
+
+            codiceSuperCat = suff + dettaglio_voce.attrib['codifica_III_livello_voce']
+            superCat = dettaglio_voce.attrib['declaratoria_III_livello_voce']
+            if not codiceSuperCat in superCatList:
+                superCatList[codiceSuperCat] = superCat
+
+            codiceSuperCat = suff + dettaglio_voce.attrib['codifica_IV_livello_voce']
+            superCat = dettaglio_voce.attrib['declaratoria_IV_livello_voce']
+            if not codiceSuperCat in superCatList:
+                superCatList[codiceSuperCat] = superCat
+
             codiceSuperCat = suff + dettaglio_voce.attrib['codifica_V_livello_voce']
             superCat = dettaglio_voce.attrib['declaratoria_V_livello_voce']
+
+            if not codiceSuperCat in superCatList:
+                superCatList[codiceSuperCat] = superCat
+                codiceSuperCat = suff + dettaglio_voce.attrib['codifica_VI_livello_voce']
+                superCat = dettaglio_voce.attrib['declaratoria_VI_livello_voce']
+            if not codiceSuperCat in superCatList:
+                superCatList[codiceSuperCat] = superCat
         except:
             pass
-
-        if not codiceSuperCat in superCatList:
-            superCatList[codiceSuperCat] = superCat
-
+        ###
         try:
-            codiceSuperCat = suff + dettaglio_voce.attrib['codifica_VI_livello_voce']
-            superCat = dettaglio_voce.attrib['declaratoria_VI_livello_voce']
-        except:
+            codice = dettaglio_voce.attrib['CMPcodifica_voce']
+        except KeyError:
+            codice = dettaglio_voce.attrib['codice_voce']
+        
+        desc = dettaglio_voce.find('declaratoria_voce').text
+        
+        try:
+            um = dettaglio_voce.attrib['udm_voce']
+        except KeyError:
+            um = dettaglio_voce.attrib['unita_misura_voce']
+        
+        prezzo = float(dettaglio_voce.attrib['prezzo_voce'])
+        
+        risorse = dettaglio_voce.find('risorse')
+        mdo = ''
+        if risorse is not None:
+            for el in risorse:
+                if el.attrib['tipologia_risorsa'] == "MANODOPERA":
+                    mdo = float(el.attrib['perc_importo_tipo_risorsa'])
+                    break
+        
+        try:
+            mdo = float(dettaglio_voce.attrib['rapporto_RU_voce']) / 100
+        except (KeyError, ValueError):
             pass
 
-        if not codiceSuperCat in superCatList:
-            superCatList[codiceSuperCat] = superCat
-
-        codice = dettaglio_voce.attrib['CMPcodifica_voce']
-        desc = dettaglio_voce.find('declaratoria_voce').text
-        um = dettaglio_voce.attrib['udm_voce']
-        prezzo = float(dettaglio_voce.attrib['prezzo_voce'])
-        risorse = dettaglio_voce.find('risorse')
-
-        for el in risorse:
-            if el.attrib['tipologia_risorsa'] == "MANODOPERA":
-                mdo = float (el.attrib['perc_importo_tipo_risorsa'])
-                break
-            else:
-                mdo = ''
         artList[codice] = {
             'codice': codice,
             'desc': desc,
@@ -109,11 +132,105 @@ def parseXML(data, defaultTitle=None):
             'mdo': mdo,
             'sicurezza': ''
         }
+    
     # ritorna un dizionario contenente tutto il necessario
     # per costruire l'elenco prezzi
     return {
         'titolo': titolo,
         'superCategorie': superCatList,
         'categorie': catList,
-        'articoli' : artList
+        'articoli': artList
+    }
+
+########################################################################
+
+def parseXML1(data, defaultTitle=None):
+    '''
+    estrae dal file XML i dati dell'elenco prezzi
+    I dati estratti avranno il formato seguente:
+
+        articolo = {
+            'codice': codice,
+            'desc': desc,
+            'um': um,
+            'prezzo': prezzo,
+            'mdo': mdo,
+            'sicurezza': oneriSic
+        }
+        artList = { codice : articolo, ... }
+
+        superCatList = { codice : descrizione, ... }
+        catList = { codice : descrizione, ... }
+
+        dati = {
+            'titolo': titolo,
+            'superCategorie': superCatList,
+            'categorie': catList,
+            'articoli' : artList
+        }
+    '''
+
+    # elimina i namespaces dai dati ed ottiene
+    # elemento radice dell' albero XML
+    root = LeenoImport.stripXMLNamespaces(data)
+    titolo = root.items()[0][-1].split('.')[0]
+
+    # ~ voci = root.find('Parte3')
+    voci = list(root.getchildren())
+            # ~ 'codice': codice,
+            # ~ 'desc': desc,
+            # ~ 'um': um,
+            # ~ 'prezzo': prezzo,
+            # ~ 'mdo': mdo,
+            # ~ 'sicurezza': ''
+    artList = {}
+    superCatList = {}
+    catList = {}
+    madre = ''
+    for voce in voci:
+        codice = voce.find('Codice').text
+        if ' - ' in codice:
+            codice = codice.split(' - ')[0]
+        try:
+            desc = PL.clean_text(voce.find('Declaratoria').text)
+        except:
+            desc = ''
+        # ~ try:
+            # ~ if desc[-1] == ':':
+                # ~ madre = desc
+            # ~ else:
+                # ~ desc = madre + '\n' + desc
+        # ~ except:
+            # ~ pass
+        try:
+            um = voce.find('UM').text
+            desc = madre + '\n' + desc
+        except:
+            um = ''
+            madre = desc
+        try:
+            prezzo = float(voce.find('Prezzo').text)
+        except:
+            prezzo = ''
+        try:
+            mdo = float(voce.find('Rapporto_RU').text)
+        except:
+            mdo = ''
+
+        artList[codice] = {
+            'codice': codice,
+            'desc': desc,
+            'um': um,
+            'prezzo': prezzo,
+            'mdo': mdo,
+            'sicurezza': ''
+        }
+    
+    # ritorna un dizionario contenente tutto il necessario
+    # per costruire l'elenco prezzi
+    return {
+        'titolo': titolo,
+        'superCategorie': superCatList,
+        'categorie': catList,
+        'articoli': artList
     }

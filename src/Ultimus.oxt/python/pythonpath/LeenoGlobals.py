7fd867af0b78ccbcde82bcc299a88f5b848e3e29
Giuseppe Vizziello <giuserpe@giuserpe.it>
2024-11-17 17:38:22 +0100

    Aggiunge funzione dest() in LeenoGlobals per gestione percorsi
    - Implementata dest() per restituire il percorso di destinazione basato sul sistema operativo.
    - Gestita creazione automatica delle directory mancanti.
    - Introdotta la variabile src_oxt per configurare dinamicamente i percorsi.
    - Migliorata la compatibilità multipiattaforma e la manutenibilità del codice.

--- src/Ultimus.oxt/python/pythonpath/LeenoGlobals.py
+++ src/Ultimus.oxt/python/pythonpath/LeenoGlobals.py
@@ -1,3 +1,4 @@
+import os
 import LeenoUtils
 
 if LeenoUtils.getGlobalVar('Lmajor') is None:
@@ -33,3 +34,36 @@ if LeenoUtils.getGlobalVar('Lmajor') is None:
 
     })
 
+# Variabile 'src_oxt' come riferimento per definizione di 'dest'
+src_oxt = '_LeenO'  # Puoi sostituirla con il valore che desideri, o lasciarla come variabile di esempio.
+
+def dest():
+    '''Definisce il percorso di destinazione basato sul sistema operativo'''
+
+    # Per sistema Windows
+    if os.name == 'nt':
+        if not os.path.exists('w:/_dwg/ULTIMUSFREE/_SRC/leeno/src/'):
+            try:
+                os.makedirs(
+                    os.getenv("HOMEPATH") + '\\' + src_oxt +
+                    '\\leeno\\src\\Ultimus.oxt\\')
+            except FileExistsError:
+                pass
+            return os.getenv("HOMEDRIVE") + os.getenv(
+                "HOMEPATH") + '\\' + src_oxt + '\\leeno\\src\\Ultimus.oxt\\'
+        else:
+            return 'w:/_dwg/ULTIMUSFREE/_SRC/leeno/src/Ultimus.oxt'
+
+    # Per sistemi Linux o macOS
+    else:
+        dest_path = '/media/giuserpe/PRIVATO/_dwg/ULTIMUSFREE/_SRC/leeno/src/Ultimus.oxt/python/pythonpath'
+        if not os.path.exists(dest_path):
+            try:
+                dest_path = os.getenv(
+                    "HOME") + '/' + src_oxt + '/leeno/src/Ultimus.oxt/'
+                os.makedirs(dest_path)
+                os.makedirs(os.getenv("HOME") + '/' + src_oxt + '/leeno/bin/')
+                os.makedirs(os.getenv("HOME") + '/' + src_oxt + '/_SRC/OXT')
+            except FileExistsError:
+                pass
+        return dest_path

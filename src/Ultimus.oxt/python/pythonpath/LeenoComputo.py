4dee9bf45c33eb791f50e39285a86aaeba9b1fe5
Giuseppe Vizziello <giuserpe@giuserpe.it>
2025-04-13 00:01:17 +0200

    Implementa la classe DatiVoce per l'estrazione e gestione strutturata dei dati da voci di computo/contabilità
    La classe fornisce:
    - Accesso organizzato ai dati da fogli COMPUTO/VARIANTE/CONTABILITÀ
    - Proprietà per accedere a singoli campi (num, art, desc, etc.)
    - Metodi interni per identificare il range delle voci ed estrarre i dati
    - Supporto per diversi stili e formati di cella
    - Gestione sia di dati di computo che contabilità

--- src/Ultimus.oxt/python/pythonpath/LeenoComputo.py
+++ src/Ultimus.oxt/python/pythonpath/LeenoComputo.py
@@ -281,3 +281,195 @@ def Menu_computoSenzaPrezzi():
         oSheet.getCellByPosition(11, lrow).String = ''
         oSheet.getCellByPosition(11, lrow).CellStyle = 'comp 1-a'
     LeenoUtils.DocumentRefresh(True)
+
+###############################################################################
+
+class DatiVoce:
+    """
+    Classe per l'estrazione e la gestione dei dati relativi a una voce di COMPUTO o CONTABILITÀ 
+    da un foglio di calcolo LibreOffice Calc.
+
+    Questa classe consente di accedere, in modo strutturato, ai dati associati a una voce, 
+    partendo da una riga specificata nel foglio. A seconda del tipo di foglio (COMPUTO, VARIANTE, 
+    CONTABILITA), estrae e organizza le informazioni in attributi distinti.
+
+    Attributi:
+        oSheet (object): Oggetto Sheet attivo, da cui leggere i dati.
+        lrow (int): Riga iniziale di riferimento per individuare la voce.
+        range_voce (object): Oggetto CellRange corrispondente alla voce (o None se non trovata).
+        SR (int): Riga iniziale della voce.
+        ER (int): Riga finale della voce.
+        voce (tuple): Dati voce computo/variante.
+        REG (tuple): Dati registro della contabilità.
+        SAL (tuple): Dati SAL della contabilità.
+
+    Utilizzo:
+        dv = DatiVoce(oSheet, lrow)
+        print(dv.SR)       # riga di inizio voce
+        print(dv.voce)     # dati voce per COMPUTO
+        print(dv.REG)      # dati registro per CONTABILITA
+        print(dv.SAL)      # dati SAL per CONTABILITA
+    """
+
+    def __init__(self, oSheet, lrow):
+        self.oSheet = oSheet
+        self.lrow = lrow
+        self._range = None
+        self._SR = None
+        self._ER = None
+        self._REG = None
+        self._SAL = None
+        self._voce = None
+
+    @property
+    def range(self):
+        if self._range is None:
+            self._range = self._circoscrive_voce()
+        return self._range
+
+    @property
+    def SR(self):
+        if self._SR is None and self.range:
+            self._SR = self.range.RangeAddress.StartRow
+        return self._SR
+
+    @property
+    def ER(self):
+        if self._ER is None and self.range:
+            self._ER = self.range.RangeAddress.EndRow
+        return self._ER
+
+    @property
+    def voce(self):
+        if self._voce is None:
+            self._estrai_dati()
+        return self._voce
+
+    @property
+    def num(self):
+        if self._voce is None:
+            self._estrai_dati()
+        return self._voce[0]
+
+    @property
+    def art(self):
+        if self._voce is None:
+            self._estrai_dati()
+        return self._voce[1]
+
+    @property
+    def desc(self):
+        if self._voce is None:
+            self._estrai_dati()
+        return self._voce[2]
+
+    @property
+    def REG(self):
+        if self._REG is None:
+            self._estrai_dati()
+        return self._REG
+
+    @property
+    def SAL(self):
+        if self._SAL is None:
+            self._estrai_dati()
+        return self._SAL
+
+    def _circoscrive_voce(self):
+        oSheet = self.oSheet
+        lrow = self.lrow
+        start_row = lrow
+        end_row = lrow
+
+        scritte_stili = {'Livello-0-scritta', 'Livello-1-scritta', 'livello2 valuta'}
+        comp_stili = {
+            'comp progress', 'comp 10 s',
+            'Comp Start Attributo', 'Comp End Attributo',
+            'Comp Start Attributo_R', 'comp 10 s_R',
+            'Comp End Attributo_R'
+        }
+
+        while oSheet.getCellByPosition(0, lrow).CellStyle in scritte_stili:
+            DLG.chi(oSheet.getCellByPosition(0, lrow).CellStyle)
+            lrow += 1
+            if lrow >= oSheet.Rows.Count:
+                return None
+
+        cs = oSheet.getCellByPosition(0, lrow).CellStyle
+
+        if cs in comp_stili.union(scritte_stili):
+            y = lrow
+            while oSheet.getCellByPosition(0, y).CellStyle not in {'Comp End Attributo', 'Comp End Attributo_R'}:
+                y += 1
+                if y >= oSheet.Rows.Count:
+                    return None
+            end_row = y
+
+            y = lrow
+            while y > 0 and oSheet.getCellByPosition(0, y).CellStyle not in {'Comp Start Attributo', 'Comp Start Attributo_R'}:
+                y -= 1
+            start_row = y
+
+        elif cs == 'Ultimus_centro_bordi_lati':
+            for y in reversed(range(0, lrow)):
+                if oSheet.getCellByPosition(0, y).CellStyle != 'Ultimus_centro_bordi_lati':
+                    start_row = y + 1
+                    break
+            for y in range(lrow, SheetUtils.getLastUsedRow(oSheet)):
+                if oSheet.getCellByPosition(0, y).CellStyle != 'Ultimus_centro_bordi_lati':
+                    end_row = y - 1
+                    break
+
+        elif 'ULTIMUS' in cs:
+            start_row = LeenoSheetUtils.cercaUltimaVoce(oSheet) + 2
+            end_row = LeenoSheetUtils.rRow(oSheet) - 1
+
+        else:
+            return cs
+
+        return oSheet.getCellRangeByPosition(0, start_row, 50, end_row)
+
+    def _estrai_dati(self):
+        if not self.range:
+            return
+
+        sr = self.SR
+        er = self.ER
+        oSheet = self.oSheet
+
+        num = oSheet.getCellByPosition(0, sr + 1).String
+        art = oSheet.getCellByPosition(1, sr + 1).String
+        desc = oSheet.getCellByPosition(2, sr + 1).String
+        quantP = oSheet.getCellByPosition(9, er).Value
+        mdo = oSheet.getCellByPosition(30, er).Value
+        sic = oSheet.getCellByPosition(17, er).Value
+
+        if oSheet.Name == 'CONTABILITA':
+            quantN = ''
+            if quantP < 0:
+                quantN = quantP
+                quantP = ''
+
+            data = oSheet.getCellByPosition(1, sr + 2).String
+            um = oSheet.getCellByPosition(8, er).String.split('[')[-1].split(']')[0]
+            Nlib = int(oSheet.getCellByPosition(19, sr + 1).Value)
+            Plib = int(oSheet.getCellByPosition(20, sr + 1).Value)
+            flag = oSheet.getCellByPosition(22, sr + 1).String
+            nSal = int(oSheet.getCellByPosition(23, sr + 1).Value)
+            prezzo = oSheet.getCellByPosition(13, er).Value
+            importo = oSheet.getCellByPosition(15, er).Value
+
+            self._REG = (
+                num + '\n' + art + '\n' + data, desc, Nlib, Plib, um,
+                quantP, quantN, prezzo, importo
+            )
+            quant = quantP if quantP != '' else quantN
+            self._SAL = (num, art, desc, um, quant, prezzo, importo, sic, mdo)
+
+        elif oSheet.Name in ('COMPUTO', 'VARIANTE'):
+            um = oSheet.getCellByPosition(8, er).String.split('[')[-1].split(']')[0]
+            prezzo = oSheet.getCellByPosition(11, er).Value
+            importo = oSheet.getCellByPosition(18, er).Value
+
+            self._voce = (num, art, desc, um, quantP, prezzo, importo, sic, mdo)
+

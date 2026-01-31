f805a290266e11d626a43390e24671b7103a0e63
giuseppe vizziello <giuserpe@giuserpe.it>
2026-01-31 21:30:45 +0100

    refactor: rimossa logica obsoleta in LeenoVariante.py
    - Eliminata la vecchia implementazione commentata di generaVariante.
    - Rimosso il codice legacy per la gestione manuale dei NamedRanges.
    - Pulizia dei moduli importati e snellimento del file.

--- src/Ultimus.oxt/python/pythonpath/LeenoVariante.py
+++ src/Ultimus.oxt/python/pythonpath/LeenoVariante.py
@@ -6,52 +6,6 @@ import pyleeno as PL
 import LeenoUtils
 import LeenoEvents
 
-# def generaVariante(oDoc, clear):
-#     '''
-#     Genera il foglio di VARIANTE a partire dal COMPUTO
-#     oDoc    documento di lavoro
-#     clear   boolean, se True cancella la variante,
-#             se false copia dal computo
-#     ritorna il foglio contenente la variante
-#     '''
-#     if not oDoc.getSheets().hasByName('VARIANTE'):
-#         if oDoc.NamedRanges.hasByName("AA"):
-#             oDoc.NamedRanges.removeByName("AA")
-#             oDoc.NamedRanges.removeByName("BB")
-
-#         oSheetComputo = oDoc.getSheets().getByName("COMPUTO")
-#         with LeenoUtils.ProtezioneFoglioContext("COMPUTO", oDoc=oDoc) as oSheetComputo:
-#             oSheet = oDoc.getSheets().getByName('COMPUTO')
-#             idx = oSheet.RangeAddress.Sheet + 1
-#             oDoc.Sheets.copyByName('COMPUTO', 'VARIANTE', idx)
-
-#         oSheet = oDoc.getSheets().getByName('COMPUTO')
-#         lrow = SheetUtils.getUsedArea(oSheet).EndRow
-#         SheetUtils.NominaArea(oDoc, 'COMPUTO', '$AJ$3:$AJ$' + str(lrow), 'AA')
-#         SheetUtils.NominaArea(oDoc, 'COMPUTO', '$N$3:$N$' + str(lrow), "BB")
-#         SheetUtils.NominaArea(oDoc, 'COMPUTO', '$AK$3:$AK$' + str(lrow), "cEuro")
-#         oSheet = oDoc.getSheets().getByName('VARIANTE')
-#         SheetUtils.setTabColor(oSheet, 16777062)
-#         oSheet.getCellByPosition(2, 0).String = "VARIANTE"
-#         oSheet.getCellByPosition(2, 0).CellStyle = "comp Int_colonna"
-#         oSheet.getCellRangeByName("C1").CellBackColor = 16777062
-#         oSheet.getCellRangeByPosition(0, 2, 42, 2).CellBackColor = 16777062
-
-#         # se richiesto, svuota la variante appena generata
-#         if clear:
-#             lrow = SheetUtils.uFindStringCol('TOTALI COMPUTO', 2, oSheet) - 3
-#             oSheet.Rows.removeByIndex(3, lrow)
-#             LeenoComputo.insertVoceComputoGrezza(oSheet, 2)
-
-#             # @@ PROVVISORIO !!!
-#             PL._gotoCella(1, 2 + 1)
-
-#             LeenoSheetUtils.adattaAltezzaRiga(oSheet)
-#     else:
-#         oSheet = oDoc.getSheets().getByName('VARIANTE')
-
-#     return oSheet
-
 import Dialogs
 
 
